831a054b63b78f6a5ee6352c07593eb3d5db13b3
Hariri A. <110659046+haririabd@users.noreply.github.com>
2025-08-20 08:00:54 +0000

    added more

--- src/pdf_generator/forms.py
+++ src/pdf_generator/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class certificateForm(forms.Form):
+class sijilKeahlianForm(forms.Form):
     name = forms.CharField(label="Nama Penuh", max_length=100)
     date = forms.CharField(label="Tarikh Pengesahan", max_length=100)
     noahli = forms.CharField(label="No Keahlian", max_length=10)
--- src/pdf_generator/views.py
+++ src/pdf_generator/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
+def formSijilKeahlian(request):
+    m = ''
+    
+    if request.method == 'POST':
+        form = forms.sijilKeahlianForm(request.POST)
+        if form.is_valid():
+            m = messages.success(request, 'Info sent successfully!')
+        else:
+            print(form.errors.as_text)
+            m = messages.error(request, 'Error in form data!')
+    else:
+        form = forms.sijilKeahlianForm()
+    
+    context = {
+        "message": m,
+        "form": form,
+        "sijil_page": 'active',
+    }
+    return render(request, html_template, context)
