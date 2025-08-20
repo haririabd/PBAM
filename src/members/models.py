b8ede781e58226f1f25be41976f35962e7891eca
Hariri A. <110659046+haririabd@users.noreply.github.com>
2025-08-20 09:54:11 +0000

    added model

--- src/members/models.py
+++ src/members/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django import forms
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    icnumber = models.CharField(max_length=12, blank=True, null=False, help_text='Enter ic number', primary_key=True)
+    notelefon = models.CharField(max_length=11, blank=True, null=True, help_text='Enter phone number')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['user', 'icnumber', 'notelefon']
+        widgets = {
+            'icnumber': forms.TextInput(attrs={
+                'class': "form-control form-control-sm",
+                'placeholder': "800101032122"
+            }),
+            'notelefon': forms.TextInput(attrs={
+                'class': "form-control form-control-sm",
+                'placeholder': "601234567890"
+            })
+        }
